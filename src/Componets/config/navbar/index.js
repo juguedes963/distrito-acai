193163556f347581470ea472ac2a9026fe4ff8ae
juguedes963 <juguedes963@github.com>
2020-07-15 09:19:43 -0300

    config env

--- src/Componets/config/navbar/index.js
+++ src/Componets/config/navbar/index.js
@@ -1,45 +1,52 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 import { Navbar, Nav, NavDropdown, Card, Accordion, useAccordionToggle } from 'react-bootstrap'
 import Logo from '../../../assets/Logo.png'
 
 export default function NavbarHeader(eventClick) {
   let conteudoButtons = ['Home', 'Curiosidade', 'Produtos', 'Login', "Admistração"]
-const [accordion,setAccordion]=useState('')
+  const [accordion, setAccordion] = useState('')
 
   return (
- 
-      <header>
+
+    <header>
 
 
 
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" style={{ backgroundColor: "#4f1233b5!important" }}>
-          <Navbar.Brand href="#home">  <div className="logo">
-            <img src={Logo} alt="logo" width={120} />
-          </div></Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mr-auto">
-              {conteudoButtons.map(sessao => {
-             
-                
-             
-                return (
-                  <Nav.Link key={sessao.toString()} href="#" onClick={event=>setAccordion({sessao})}> {sessao}</Nav.Link>
-                );
-              })}
-            </Nav>
-
-          </Navbar.Collapse>
-        </Navbar>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" style={{ backgroundColor: "#4f1233b5!important" }}>
+        <Navbar.Brand href="#home">  <div className="logo">
+          <img src={Logo} alt="logo" width={120} />
+        </div></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            {conteudoButtons.map(sessao => {
 
 
-      </header>
 
-      
-       
-       
+              return (
+                <Nav.Link key={sessao.toString()} href="#" onClick={event => { conteudoLabel(setAccordion({ sessao })) }}> {sessao}</Nav.Link>
+              );
+            })}
+          </Nav>
+
+        </Navbar.Collapse>
+      </Navbar>
+
+
+    </header>
+
+
+
+
   )
 }
+function conteudoLabel(event) {
+  return (
+    <p>
+      {event}
+    </p>
 
+  )
+}
 
